4a9eacb06d3386d036a7b55cdd3329496a756866
cacau <anaclaudia.sza@outlook.com>
2022-08-03 12:04:49 -0300

    chore: erro requisito cinco

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const { readFile } = require('./fileSystem');
-const validateUser = require('./userValidation');
+const {
+  validateUser,
+  validateTalkDate,
+  validateTalker,
+  validateToken,
+  validateRate,
+} = require('./userValidation');
 
 const app = express();
 app.use(bodyParser.json());
@@ -39,6 +45,26 @@ app.post('/login', validateUser, (_request, response) => {
   const token = crypto.randomBytes(8).toString('hex');
 
   return response.status(200).json({ token });
+});
+
+app.post('/talker', validateTalker, validateToken, validateTalkDate,
+  validateRate, async (request, response) => {
+  // const { name, age, talk } = request.body;
+  const talkers = await readFile();
+
+  talkers.push({
+    id: talkers.length + 1,
+    ...request.body,
+    // name,
+    // age,
+    // talk: {
+    //   rate: talk.rate,
+    //   watchedAt: talk.watchedAt,
+    // },
+  });
+  console.log(talkers[talkers.length - 1]);
+
+  return response.status(201).json(talkers[talkers.length - 1]);
 });
 
 app.listen(PORT, () => {
--- userValidation.js
+++ userValidation.js
@@ -19,4 +19,75 @@ function validateUser(request, response, next) {
   next();
 }
 
-module.exports = validateUser;
+function validateToken(request, response, next) {
+  const { authorization } = request.headers;
+
+  if (!authorization) {
+    return response.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) {
+    return response.status(401).json({ message: 'Token inválido' });
+  }
+
+  next();
+}
+
+function validateTalker(request, response, next) {
+  const { name, age } = request.body;
+
+  if (!name) {
+    return response.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return response.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  if (!age) {
+    return response.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (Number(age) < 18) {
+    return response.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+}
+
+function validateTalkDate(request, response, next) {
+  const { talk } = request.body;
+  const dateRegex = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+  const dataValid = dateRegex.test(talk.watchedAt);
+
+  if (!talk.watchedAt) {
+    return response.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+  if (!dataValid) {
+    return response.status(400).json({
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+
+  next();
+}
+
+function validateRate(request, response, next) {
+  const { talk } = request.body;
+
+  if (!talk) {
+    return response.status(400).json({ message: 'O campo "talk" é obrigatório' });
+  }
+  if (!talk.rate) {
+    return response.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  }
+  if (talk.rate < 1 || talk.rate > 5) {
+    return response.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+}
+
+module.exports = {
+  validateUser,
+  validateTalker,
+  validateToken,
+  validateRate,
+  validateTalkDate,
+};
